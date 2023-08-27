e99982ee32c53e80e0f7c5a1df4f40b748e61410
Benn Christensen <benn.christensen@wolterskluwer.com>
2023-08-27 14:15:48 +0200

    prompt for input

--- src/RegistrationFee.java
+++ src/RegistrationFee.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 public class RegistrationFee {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        System.out.print("Indtast nypris: ");
+        int price = input.nextInt();
+        double registrationFee = calculateFee(price);
+        System.out.println("En bil der koster " + price + " uden registreringsafgift, skal man betale " +
+                registrationFee + " i registreringsafgift.");
     }
 
-    public static double CalculateFee(int price) {
+    public static double calculateFee(int price) {
         return 0;
     }
 }
